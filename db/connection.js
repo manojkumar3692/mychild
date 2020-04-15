25be0cb0990c20d9e82b0f3cb403f0ea4079ed8f
Manoj Kumar <manoj@thelibratech.com>
2020-04-16 01:01:34 +0400

    added prod db

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,13 @@
 const Sequelize = require('sequelize');
 const db = {}
-const sequelize = new Sequelize('minieli', '', 'root@123', {
-    host: 'localhost',
-    dialect: 'postgres',
-  })
+// const sequelize = new Sequelize('minieli', '', 'root@123', {
+//     host: 'localhost',
+//     dialect: 'postgres',
+//   })
+const sequelize = new Sequelize('ddod75qhpe303j', 'zzbxixavcsscsh', '132db0f0be587ff2a8759d374eca35e520e944f38e53ec81ddf5da696b97db75', {
+  host: 'ec2-52-87-58-157.compute-1.amazonaws.com',
+  dialect: 'postgres',
+})
   sequelize
   .authenticate()
   .then(() => {
